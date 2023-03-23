8ec9ae6cd8761ff0193db071f9102619abb53756
mic050r <s2216@e-mirim.hs.kr>
2023-03-23 14:11:34 +0900

    오버라이딩과 super 키워드

---
+++ src/in08/Circle.java
@@ -0,0 +1,18 @@
+package in08;
+
+public class Circle extends Shape {
+	protected String name;
+
+	@Override
+	public void draw() {
+		name = "Circle";
+		super.name = "Shape";
+		super.draw();
+		System.out.println(name);
+	}
+
+	public static void main(String args[]) {
+		Shape b = new Circle();
+		b.paint();
+	}
+}
