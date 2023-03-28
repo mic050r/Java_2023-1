f9b940a550f5be2a2aeeb6f3777491427eda2835
mic050r <s2216@e-mirim.hs.kr>
2023-03-28 10:03:18 +0900

    예제 1-8 : 인터페이스 구현

---
+++ src/in10/InterfaceEX.java
@@ -0,0 +1,13 @@
+package in10;
+
+public class InterfaceEX {
+
+	public static void main(String[] args) {
+		SamsungPhone phone = new SamsungPhone();
+		phone.printfLog();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+
+}
